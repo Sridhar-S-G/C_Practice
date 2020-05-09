57d91ceffe97288fcd2a8761103f8e126a7fa8cb
Sridhar-S-G <sridharsg.official.98@gmail.com>
2020-05-10 04:47:09 +0530

    first commit

---
+++ Patterns/HaflPyramidOfAsterisk.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int n_rows;
+    printf("Enter the number of Rows needed\n\n");
+    scanf("%d",&n_rows);
+    for(int i=1;i<=n_rows;i++){
+        for(int j=1;j<=i;j++)
+            printf("* ");
+        printf("\n");
+    }
+}
